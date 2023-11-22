86f37f4b76a1e673523ad9ae9dec2255ca218342
Mubashir P A <mubashirpa2002@gmail.com>
2023-11-22 11:26:42 +0530

    Upgrade dependencies and Gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.9.10"
-    id("io.ktor.plugin") version "2.3.4"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+    kotlin("jvm") version "1.9.20"
+    id("io.ktor.plugin") version "2.3.6"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20"
 }
 
 group = "com.example"
